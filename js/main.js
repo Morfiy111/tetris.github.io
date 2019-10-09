61eeec7a94d770e09a6df0798663bf296a7fd3c0
morfiy <edurnev@keaz.ru>
2019-10-09 10:58:35 +0300

    fix

--- js/main.js
+++ js/main.js
@@ -1,25 +1,15 @@
-var itemsCount=3;
-if (window.matchMedia("(max-width: 991px)").matches) {
-    itemsCount=2;
-}
-if (window.matchMedia("(max-width: 767px)").matches) {
-    itemsCount=1;
-}
-var owl = $('.owl-carousel');
-owl.owlCarousel({
-    items:itemsCount,
-    loop:true,
-    margin:0,
-    nav:true,
-    autoplay:true,
-    autoplayTimeout:3000,
-    autoplayHoverPause:true
+$(document).ready(function(){
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 50) {
+            $('#button-up').fadeIn();
+        } else {
+            $('#button-up').fadeOut();
+        }
+    });
+    $('#button-up').click(function () {
+        $('body,html').animate({
+            scrollTop: 0
+        }, 500);
+        return false;
+    });
 });
-$('.play').on('click',function(){
-    owl.trigger('autoplay.play.owl',[3000])
-})
-$('.stop').on('click',function(){
-    owl.trigger('autoplay.stop.owl')
-})
-
-
