b0c1543cd7b939cbc40a275ffba6373f3b21f39c
morfiy <edurnev@keaz.ru>
2019-10-11 09:47:44 +0300

    fix

--- js/main.js
+++ js/main.js
@@ -35,3 +35,25 @@ $(document).ready(function () {
         $(this).toggleClass("active");
     });
 });
+
+var itemsCount=2;
+
+if (window.matchMedia("(max-width: 767px)").matches) {
+    itemsCount=1;
+}
+var owl = $('.owl-carousel');
+owl.owlCarousel({
+    items:itemsCount,
+    loop:true,
+    margin:0,
+    nav:true,
+    autoplay:true,
+    autoplayTimeout:3000,
+    autoplayHoverPause:true
+});
+$('.play').on('click',function(){
+    owl.trigger('autoplay.play.owl',[3000])
+})
+$('.stop').on('click',function(){
+    owl.trigger('autoplay.stop.owl')
+})
